0a043db30f080d7b7995e582cfdd58c8ff43af5a
yjkim <31876648+jcdad3000@users.noreply.github.com>
2022-10-30 23:37:42 +0900

    주문 서비스 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,12 +1,15 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -23,6 +26,7 @@ public class OrderItem {
 
     private int orderPrice;
     private int count;
+
 
     //==생성 메서드==//
     public static OrderItem createOrderItem(Item item, int orderPrice,int count){
